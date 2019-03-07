692a9cdc81509c5f7285a2a66cf76e64824c525c
max ortman <mortman2299@stu.d214.org>
2019-03-07 14:53:10 -0600

    DJKN

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -6,18 +6,22 @@
 //  Copyright © 2019 john hersey high school. All rights reserved.
 //
 
+
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        
     }
-
-
 }
 
